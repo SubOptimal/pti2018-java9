35791d0615de97e0cea1305be33b9856d5034259
Frank Dietrich <bits_n_bytes@gmx.de>
2018-03-26 14:21:21 +0200

    amend Debian package example

--- modular-and-runtime-images/07-jre-image-packaged/01-javapackager.sh
+++ modular-and-runtime-images/07-jre-image-packaged/01-javapackager.sh
@@ -6,6 +6,9 @@ javapackager -deploy \
 	-native deb \
 	-BsignBundle=false \
 	-BappVersion=1.0 \
+	"-Bvendor=Frank Dietrich" \
+	"-Bemail=bits_n_bytes@gmx.de" \
+	"-BlicenseType=GPL v2 + Classpath Exception" \
     -BjlinkOptions=compress=2 \
 	--module-path mlib \
 	--module com.greetings/com.greetings.Main
